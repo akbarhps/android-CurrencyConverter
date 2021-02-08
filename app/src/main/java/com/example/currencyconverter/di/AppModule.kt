383babe7bcc59b2e2c241185b00c5840f67afb54
akbarhps <akbarputrasrg@gmail.com>
2021-02-08 18:32:11 +0700

    📦 Add provideMainRepository and provideDispatchers

--- app/src/main/java/com/example/currencyconverter/di/AppModule.kt
+++ app/src/main/java/com/example/currencyconverter/di/AppModule.kt
@@ -1,11 +1,15 @@
 package com.example.currencyconverter.di
 
 import com.example.currencyconverter.data.CurrencyServices
+import com.example.currencyconverter.main.MainRepository
 import com.example.currencyconverter.util.Constants
+import com.example.currencyconverter.util.DispatcherProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -20,5 +24,24 @@ object AppModule {
         .baseUrl(Constants.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build().create(CurrencyServices::class.java)
+
+    @Singleton
+    @Provides
+    fun provideMainRepository(
+        services: CurrencyServices
+    ): MainRepository = MainRepository(services)
+
+    @Singleton
+    @Provides
+    fun provideDispatchers(): DispatcherProvider = object : DispatcherProvider {
+        override val main: CoroutineDispatcher
+            get() = Dispatchers.Main
+        override val io: CoroutineDispatcher
+            get() = Dispatchers.IO
+        override val default: CoroutineDispatcher
+            get() = Dispatchers.Default
+        override val unconfined: CoroutineDispatcher
+            get() = Dispatchers.Unconfined
+    }
 
 }
